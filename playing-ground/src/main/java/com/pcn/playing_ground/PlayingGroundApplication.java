2f5b223718388d1da2705088e10f1f79be475874
Bum Chi Ni <phamchinghi11@gmail.com>
2024-09-30 00:14:38 +0700

    init

---
+++ playing-ground/src/main/java/com/pcn/playing_ground/PlayingGroundApplication.java
@@ -0,0 +1,13 @@
+package com.pcn.playing_ground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingGroundApplication.class, args);
+	}
+
+}
---
+++ playing-ground/src/test/java/com/pcn/playing_ground/PlayingGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcn.playing_ground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayingGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
